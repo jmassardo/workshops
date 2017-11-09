51dac62ffe185de459fb4af4f7c1551ebe507a4d
James Massardo <james@dxrf.com>
2017-11-09 17:14:42 -0600

    Added base framework to support ubuntu

--- 1_MongoDB/Install_MongoDB/recipes/default.rb
+++ 1_MongoDB/Install_MongoDB/recipes/default.rb
@@ -3,35 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-
-# Create yum repo folder if it doesn't exist
-# More info here: https://docs.chef.io/resource_directory.html
-directory '/etc/yum.repos.d/' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-# Populate the file from a template. Using a template keeps the recipe cleaner
-# More info here: https://docs.chef.io/resource_template.html
-template '/etc/yum.repos.d/mongodb-org-3.4.repo' do
-  source 'mongo_repo.erb'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-# Install MongoDB
-# More info here: https://docs.chef.io/resource_package.html
-package 'Install MongoDB' do
-  package_name 'mongodb-org'
-  action :install
-end
-
-# Make sure the service is started and configured to run on a system restart
-# More info here: https://docs.chef.io/resource_service.html
-service 'mongod' do
-  action [ :enable, :start ]
+case node["platform"]
+when "centos"
+  include_recipe 'Install_MongoDB::mongodb_install_centos'
+when "ubuntu"
+  include_recipe 'Install_MongoDB::mongodb_install_ubuntu'
 end
---
+++ 1_MongoDB/Install_MongoDB/recipes/mongodb_install_ubuntu.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: Install_MongoDB
+# Recipe:: mongodb_install_ubuntu
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
