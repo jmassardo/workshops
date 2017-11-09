b32d45b1dce9b5582893048ec9c5e54366dd71d5
James Massardo <james@dxrf.com>
2017-11-09 14:51:37 -0600

    Created base cookbook for installing tomcat

---
+++ 2_Tomcat/Install_Tomcat/README.md
@@ -0,0 +1,4 @@
+# Install_Tomcat
+
+TODO: Enter the cookbook description here.
+
---
+++ 2_Tomcat/Install_Tomcat/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: Install_Tomcat
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
