3c3215f2e5f8c6a063a782bf5c860497595abe9f
James Massardo <james@dxrf.com>
2017-11-08 18:12:28 -0600

    Create yum repo from template

--- 1_MongoDB/Install_MongoDB/recipes/default.rb
+++ 1_MongoDB/Install_MongoDB/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+#Create yum repo file so it knows how to find the mongodb packages
+
+template '/etc/yum.repos.d/mongodb-org-3.4.repo' do
+  source 'mongo_repo.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
