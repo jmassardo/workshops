1ab857ec8ee6e40ef3fe1b56a22c97e1a837f6a7
James Massardo <james@dxrf.com>
2017-11-08 19:01:37 -0600

    Mongo repo resources passing inspec tests. Moving to next item

--- 1_MongoDB/Install_MongoDB/recipes/default.rb
+++ 1_MongoDB/Install_MongoDB/recipes/default.rb
@@ -6,6 +6,13 @@
 
 #Create yum repo file so it knows how to find the mongodb packages
 
+directory '/etc/yum.repos.d/' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 template '/etc/yum.repos.d/mongodb-org-3.4.repo' do
   source 'mongo_repo.erb'
   owner 'root'
