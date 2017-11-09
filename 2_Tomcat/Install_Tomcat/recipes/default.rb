3690ace5adfa44e04f9fcc8243bd4f838e69ad13
James Massardo <james@dxrf.com>
2017-11-09 15:02:47 -0600

    added resource and test for Java install

--- 2_Tomcat/Install_Tomcat/recipes/default.rb
+++ 2_Tomcat/Install_Tomcat/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install Java
+# More info here: https://docs.chef.io/resource_package.html
+package 'Install Java 1.7' do
+    package_name 'java-1.7.0-openjdk-devel'
+    action :install
+  end
