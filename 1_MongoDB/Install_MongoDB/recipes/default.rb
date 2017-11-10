710879fedb1052990805ce1d86f046df11b5b515
James Massardo <james@dxrf.com>
2017-11-10 09:10:45 -0600

    updated mongo cookbook to use community cookbook and removed unneeded test files

--- 1_MongoDB/Install_MongoDB/recipes/default.rb
+++ 1_MongoDB/Install_MongoDB/recipes/default.rb
@@ -3,9 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-case node["platform"]
-when "centos"
-  include_recipe 'Install_MongoDB::mongodb_install_centos'
-when "ubuntu"
-  include_recipe 'Install_MongoDB::mongodb_install_ubuntu'
-end
+
+# install mongodb using the community cookbook
+include_recipe "sc-mongodb::default"
+
+# case node["platform"]
+# when "centos"
+#   include_recipe 'Install_MongoDB::mongodb_install_centos'
+# when "ubuntu"
+#   include_recipe 'Install_MongoDB::mongodb_install_ubuntu'
+# end
--- 1_MongoDB/Install_MongoDB/test/smoke/default/default_test.rb
+++ 1_MongoDB/Install_MongoDB/test/smoke/default/default_test.rb
@@ -5,29 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-# Some tests are different between centos and ubuntu so this separates those items
-if os.debian?
-  # Make sure the key got imported
-  describe command('apt-key list') do
-    its('stdout') { should match (/MongoDB/) }
-  end
-  
-  # Make sure the source file exists
-  describe file('/etc/apt/sources.list.d/mongodb-org-3.4.list') do
-    it { should exist }
-  end
-  
-elsif os.redhat?
-  # Make sure the mongodb repo file gets created
-  describe file('/etc/yum.repos.d/mongodb-org-3.4.repo') do
-    it { should exist }
-  end
-end
-
-#
-# These are common tests
-#
-
 # Make sure that mongo is installed
 describe package('mongodb-org') do
     it { should be_installed }
@@ -41,6 +18,6 @@ describe service('mongod') do
 end
 
 # Test to see if we can successfully run the mongo executable from the command line
-describe command('mongo --eval "quit()" | grep server') do
-  its('stdout') { should eq "MongoDB server version: 3.4.10\n" }
+describe command('mongo --eval "quit()"') do
+  its('stdout') { should match /MongoDB/ }
 end
