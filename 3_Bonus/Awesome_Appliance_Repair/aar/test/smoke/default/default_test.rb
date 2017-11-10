ec27edc28c6ac71a6ed009deba9d14c336114fc7
James Massardo <james@dxrf.com>
2017-11-10 10:23:59 -0600

    described initial tests

--- 3_Bonus/Awesome_Appliance_Repair/aar/test/smoke/default/default_test.rb
+++ 3_Bonus/Awesome_Appliance_Repair/aar/test/smoke/default/default_test.rb
@@ -6,15 +6,65 @@
 # found at http://inspec.io/docs/reference/resources/
 
 # Is apache2 installed?
+describe package('apache2') do
+  it { should be_installed }
+end
+
 # Is mysql installed?
+describe package('mysql') do
+  it { should be_installed }
+end
+
 # Is unzip installed?
+describe package('unzip') do
+  it { should be_installed }
+end
+
 # Did the remote file get downloaded?
+describe file('/tmp/master.zip') do
+  it { should exist }
+end
+
 # Did the install get unpacked to the proper path?
-# Are the permissions correct on the install directory?
+describe directory('/var/www/AAR') do
+  it { should exist }
+  its('owner') { should eq 'www-data' }
+end
+
 # Does the apache conf file exist?
+describe file('/etc/apache2/sites-enabled/AAR-apache.conf') do
+  it { should exist }
+end
+
+# TODO
 # -> and does it have the correct content?
+
 # Does the AAR_config file exist?
+describe file('/var/www/AAR/AAR_config.py') do
+  it { should exist}
+end
+
+# TODO
 # -> and does it have the correct content?
+
 # Is mysql running?
+describe service('mysql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+describe port(3306) do
+  it { should be_listening }
+end
 # Is apache running?
+describe service('apache2') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+describe port(80) do
+  it { should be_listening }
+end
+
+# TODO
 # Does the app load properly?
