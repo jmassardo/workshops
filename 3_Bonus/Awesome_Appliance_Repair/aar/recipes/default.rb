11d88f91392d447ae3198a5bf54247ce745caed0
James Massardo <james@dxrf.com>
2017-11-10 10:35:21 -0600

    Added package install resources

--- 3_Bonus/Awesome_Appliance_Repair/aar/recipes/default.rb
+++ 3_Bonus/Awesome_Appliance_Repair/aar/recipes/default.rb
@@ -5,8 +5,20 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 # Install apache2
+package 'apache2' do
+  action :install
+end
+
 # Install mysql
+package 'mysql-server' do
+  action :install
+end
+
 # Install unzip
+package 'unzip' do
+  action :install
+end
+
 # Download remote install file
 # Create install path and set proper ownership
 # unpack file to proper path
