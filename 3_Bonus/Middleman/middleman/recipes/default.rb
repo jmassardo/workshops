becb1b3441c044e8f4b20966191599bc32671625
James Massardo <james@dxrf.com>
2017-11-10 20:47:27 -0600

    added package install resources

--- 3_Bonus/Middleman/middleman/recipes/default.rb
+++ 3_Bonus/Middleman/middleman/recipes/default.rb
@@ -3,3 +3,36 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# update packages
+execute 'apt-get update' do
+  command 'sudo apt-get update'
+  action :run
+end
+
+# Install packages
+%w(
+    build-essential
+    libssl-dev
+    libyaml-dev
+    libreadline-dev
+    openssl
+    curl
+    git-core
+    zlib1g-dev
+    bison
+    libxml2-dev
+    libxslt1-dev
+    libcurl4-openssl-dev
+    nodejs
+    libsqlite3-dev
+    sqlite3
+    apache2
+    git
+  ).each do |pkg|
+    package "#{pkg}" do
+      action :install
+    end
+end
+
+
