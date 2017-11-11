dae4d3b6279644e4e583b149e9a6f06724e6ec38
James Massardo <james@dxrf.com>
2017-11-10 20:50:13 -0600

    resource to build ruby

--- 3_Bonus/Middleman/middleman/recipes/default.rb
+++ 3_Bonus/Middleman/middleman/recipes/default.rb
@@ -35,4 +35,20 @@ end
     end
 end
 
-
+# Install Ruby
+ruby 'Install Ruby' do
+  interpreter 'bash'
+  code <<-EOH
+    mkdir ~/ruby
+    cd ~/ruby
+    wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.3.tar.gz
+    tar -xzf ruby-2.1.3.tar.gz
+    cd ruby-2.1.3
+    ./configure
+    make install
+    rm -rf ~/ruby
+    sudo cp /usr/local/bin/ruby /usr/bin/ruby
+    sudo cp /usr/local/bin/gem /usr/bin/gem
+  EOH
+  not_if { ::File.exist?('/usr/local/bin/ruby') }
+end
