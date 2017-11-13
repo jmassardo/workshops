33c94c086b94c11ad766c80b6cbd0e01585efeef
James Massardo <james@dxrf.com>
2017-11-12 19:11:50 -0600

    added chefspec tests to mongodb cookbook

--- 1_MongoDB/Install_MongoDB/spec/unit/recipes/default_spec.rb
+++ 1_MongoDB/Install_MongoDB/spec/unit/recipes/default_spec.rb
@@ -7,16 +7,36 @@
 require 'spec_helper'
 
 describe 'Install_MongoDB::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on an Cemtos 7.4.1708' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'creates /etc/yum.repos.d directory' do
+      expect(chef_run).to create_directory('/etc/yum.repos.d/')
+    end
+
+    it 'creates mongo repo file' do
+      expect(chef_run).to create_template('/etc/yum.repos.d/mongodb-org-3.4.repo')
+    end
+
+    it 'installs mongodb' do
+      expect(chef_run).to install_package 'mongodb-org'
+    end
+  
+    it 'enables the mongodb service' do
+      expect(chef_run).to enable_service 'mongod'
+    end
+  
+    it 'starts the mongodb service' do
+      expect(chef_run).to start_service 'mongod'
     end
   end
 end
